90ba5162e032dddf40beb60f7ab5a1bacd89e2d8
Usmaelabdureman <uabdureman@gmail.com>
2023-03-21 16:06:41 +0300

    my nodejs examples

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const rect =require("./rectangle")
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
+
+solveRect(5,6)
+solveRect(5,0)
